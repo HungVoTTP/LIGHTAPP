fe16a6355a4aba66864582a4dae14e4569012af6
Danny <hungvo.23042008@gmail.com>
2024-06-16 11:31:25 +0700

    Initial Commit

---
+++ LIGHTAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LIGHTAPP
+//
+//  Created by MacOs on 16/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
